cb9588f5c5c01d9a8da4b116d693ebb56332a0cd
San4o9910 <98598019+San4o9910@users.noreply.github.com>
2026-07-30 20:00:28 +0300

    Add demo kitchen data

---
+++ app/src/data/demoKitchen.js
@@ -0,0 +1,20 @@
+export const freezer = [
+  {
+    name: 'Куриные котлеты',
+    quantity: 24,
+    unit: 'шт',
+    status: '🟢 доступно'
+  },
+  {
+    name: 'Тефтели',
+    quantity: 18,
+    unit: 'шт',
+    status: '🟢 доступно'
+  }
+];
+
+export const todayMeal = {
+  title: 'Куриные котлеты с гречкой',
+  time: '25 минут',
+  freezer: 'Пакет №024'
+};
